663c7f6dbca5a709c42eb81622d91479f4042101
Paul Bordea <paul.bordea@ymail.com>
2017-01-17 00:42:09 +0200

    Introduce observables

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,13 +1,27 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
 import {Component} from 'angular2/core';
-import {ChangePasswordFormComponent} from '../../components/change-password-form/change-password-form.component';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
     selector: 'my-app',
     template: `
-        <change-password-form></change-password-form>
-    `,
-    directives: [ChangePasswordFormComponent]
+        <input id="search" type="text" class="form-control">
+    `
 })
-
 export class AppComponent { 
+    constructor() {
+        var keyups = Observable.fromEvent($("#search"), "keyup")
+            .map(e => e.target.value)
+            .filter(text => text.length >=3)
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .flatMap(searchTerm => {
+                var url = "https://api.spotify.com/v1/search?type=artist&q=" + searchTerm;
+                var promise = $.getJSON(url);
+                return Observable.fromPromise(promise);
+            });
+
+        keyups.subscribe(data => console.log(data));
+    }
 }
