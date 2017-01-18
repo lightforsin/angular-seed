ac2e078c033fdd7f2e2b65cc99e285a1aeedda0f
Paul Bordea <paul.bordea@ymail.com>
2017-01-19 00:00:07 +0200

    Add Http support for calling REST services

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -2,54 +2,24 @@
 
 import {Component} from 'angular2/core';
 import {ControlGroup,FormBuilder} from 'angular2/common';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/debounceTime';
-//import 'rxjs/add/operator/fromArray';
+import {PostService} from '../../services/post.service';
+import {HTTP_PROVIDERS} from 'angular2/http';
 
 @Component({
     selector: 'my-app',
     template: `
-        <form [ngFormModel]="form">
-            <input type="text" ngControl="search">
-        </form>
-    `
+        <button id="myButton" (click)="getData()">Click here</button>
+    `,
+    providers: [PostService, HTTP_PROVIDERS]
 })
 export class AppComponent { 
-    form: ControlGroup;
-
-    constructor(fb: FormBuilder) {
-        this.form = fb.group({
-            search:[]
-        });
-
-        var search = this.form.find('search');
-
-        var changes = search.valueChanges
-            .debounceTime(400)
-            .map(str =>(<string>str).replace('','-­‐'));
-
-        changes.subscribe(x => console.log(x));
-
-        // var startDates = [];
-        // var startDate = new Date();
-
-        // for(var day=-2; day <= 2; day++){
-        //     var date = new Date(
-        //         startDate.getFullYear(),
-        //         startDate.getMonth(),
-        //         startDate.getDate() + day
-        //     );
-
-        //     startDates.push(date);
-        // }
-
-        // Observable
-        //     .fromArray(startDates)
-        //     .map(date => {
-        //         console.log("Getting deals for date " + date);
-        //         return [1, 2, 3];
-        //     })
-        //     .subscribe(x => console.log(x));
+    constructor(private _postService: PostService) {
+        
     }
+    
+    getData() {
+        this._postService.getPosts()
+            .subscribe(posts => console.log(posts));
+    }
+    
 }
