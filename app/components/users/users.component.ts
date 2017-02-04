a82a523c68bdf541d7d9271934c773370e9e947c
Paul Bordea <paul.bordea@ymail.com>
2017-02-04 21:28:37 +0200

    Extract template for users component in html file.

--- app/components/users/users.component.ts
+++ app/components/users/users.component.ts
@@ -8,32 +8,7 @@ import {SaveUserComponent} from '../../components/users/saveuser.component';
 
 @Component({
     selector: 'users',
-    template: `
-        <h1>Users</h1>
-
-        <a class="btn btn-primary" [routerLink]="['\SaveUser', {id: null}]">Add user</a>
-
-        <table class="table table-bordered">
-            <thead>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </thead>
-            <tbody>
-                <tr *ngFor="#user of users">
-                    <td>{{user.name}}</td>
-                    <td>{{user.email}}</td>
-                    <td>
-                        <a [routerLink]="['SaveUser', { id: user.id }]">
-                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-                        </a>
-                    </td>
-                    <td>DEF</td>
-                </tr>
-            </tbody>
-        </table>
-    `,
+    templateUrl: '/app/components/users/users.component.html',
     styles: [`
         th {
             color: red;
